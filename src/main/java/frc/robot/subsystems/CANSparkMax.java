9fc6a39106f5c05d1b0d5f83f0a6f4fa87708fab
AJL0715 <ainsly.leithead@gmail.com>
2026-01-29 19:41:27 -0600

    Auto- B2/B3 collect/shoot/climb

--- src/main/java/frc/robot/subsystems/CANSparkMax.java
+++ src/main/java/frc/robot/subsystems/CANSparkMax.java
@@ -1,5 +1,12 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
+
 public class CANSparkMax {
+
+    public RelativeEncoder getEncoder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEncoder'");
+    }
 
 }
