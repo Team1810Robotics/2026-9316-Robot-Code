a53b0800e9a6f9851f4794a3041eaabdb1dff97f
grat78 <tuttlgra000@usd232.org>
2026-01-31 10:30:44 -0600

    added offsets, pose estimation, apriltag id, and some helpful comments

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -4,7 +4,8 @@ import frc.robot.LimelightHelpers;
 // DriverStation not used in this subsystem
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.geometry.Pose2d;
-
+import frc.robot.Constants;
+import frc.robot.Constants.VisionConstants;
 
 /**
  * Vision subsystem adapted to use your LimelightHelpers library, auto-selecting
@@ -17,14 +18,18 @@ public class VisionSubsystem extends SubsystemBase {
     // Limelight identifier ("" for default 'limelight' table, or the hostname if using multiple)
     private final String limelightName;
 
-
-
-
     public VisionSubsystem(String name) {
         this.limelightName = name;
     }
 
+    // gets botpose estimates for both alliances
+    @Override
+    public void periodic() {
+        LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+        LimelightHelpers.getBotPoseEstimate_wpiRed(limelightName);
+    }
 
+    // gets the april tag ID
     /** @return AprilTag / fiducial ID (tid) */
     public int getTargetID() {
         return (int) LimelightHelpers.getFiducialID(limelightName);
@@ -32,11 +37,12 @@ public class VisionSubsystem extends SubsystemBase {
 
     /**
      * this is the one that matters ;)
-     * 676676767676676767676767676767676767676767676767676767676767676767676767676767676767676
      * CALL THIS NUMBER -> (913) 488-2670
      * -sam
      * @return
      */
+
+    // translates the botpose to 2d
     public Pose2d getBotPoseTargetSpace() {
         return LimelightHelpers.toPose3D(LimelightHelpers.getBotPose_TargetSpace(limelightName)).toPose2d();
     }
