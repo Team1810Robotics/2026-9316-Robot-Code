839f4a2e7492b61b9174a2cb70a94425432424d1
stensjac000usd <stensjac000@usd232.org>
2026-02-07 10:02:13 -0600

    Hood

--- src/main/java/frc/robot/commands/Hood.java
+++ src/main/java/frc/robot/commands/Hood.java
@@ -1,7 +1,14 @@
 package frc.robot.commands;
 /*
  * notes go here:
- * x44 motor
+ * 
+ * kraken x44 motor
+ * 
+ * encoder (tell where the hood is based on how many rotations it has done)
+ *   0 = hood is fully down, 1 = hood is fully up (or something like that)
+ * 
+ * up and down movement
+ * 
  */
 public class Hood {
    
