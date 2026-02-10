30443b0e2f50e72aac498e82416f064d7bec9fe3
cgathright133 <cgathright@gmail.com>
2026-02-09 21:44:00 -0600

    removed duplicate files (changed location)

--- src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
@@ -6,20 +6,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // import com.revrobotics.spark.SparkLowLevel.MotorType;
 public class ClimbSubsystem extends SubsystemBase {
-  private final SparkMax motor1 = new SparkMax(ClimbConstants.motor1ID, MotorType.kBrushless);
-
+        private final SparkMax motor1 = new SparkMax(ClimbConstants.motor1ID, null);
+		private final SparkMax motor2 = new SparkMax(ClimbConstants.motor2ID, null);
   // set_parameter(hb, "FlywheelSub", "frc.robot.subsystems.FlywheelSub");
   // new.motorcontrol = "edu.wpi.first.wpilibj.motorcontrol.Kraken";
 
   public void Extend() {
     motor1.set(0.25);
+    motor2.set(0.25);
   }
 
   public void Stop() {
     motor1.set(0.0);
+    motor2.set(0.0);
   }
 
   public void Retract() {
     motor1.set(-0.25);
+    motor2.set(-0.25);
   }
 }
