43386e833a3d55f2cd70cbcb4d24d197895c329f
AJL0715 <ainsly.leithead@gmail.com>
2026-02-26 21:08:42 -0600

    climb command

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.auto.NamedCommands;
+
 // import org.opencv.features2d.FlannBasedMatcher;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -8,22 +10,28 @@ import frc.robot.subsystems.climb.ClimbSubsystem;
 
 /** Flywheel command */
 public class Climb extends Command {
-  public static ClimbSubsystem ClimbSub = new ClimbSubsystem();
+  private final ClimbSubsystem ClimbSubsystem;
+
+  public Climb(ClimbSubsystem climbSubsystem) {
+    this.ClimbSubsystem = climbSubsystem;
+     NamedCommands.registerCommand("climb", new Climb(ClimbSubsystem, ClimbConstants.time));
+  }
 
   @Override
   public void initialize() {
-    if (ClimbSub.isExtended == false) {
-      ClimbSub.Extend();
+    
+    if (ClimbSubsystem.isExtended == false) {
+      ClimbSubsystem.Extend();
       this.withTimeout(ClimbConstants.time);
-      ClimbSub.Stop();
+      ClimbSubsystem.Stop();
 
-      ClimbSub.isExtended = true;
+      ClimbSubsystem.isExtended = true;
 
     } else {
-      ClimbSub.Retract();
+      ClimbSubsystem.Retract();
       this.withTimeout(ClimbConstants.time);
-      ClimbSub.Stop();
-      ClimbSub.isExtended = false;
+      ClimbSubsystem.Stop();
+      ClimbSubsystem.isExtended = false;
     }
   }
 
