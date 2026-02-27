b6498caedc329dd0e6bcfcae44682c32e5875fe8
RealOrxify <wyatt.lux90@gmail.com>
2026-02-26 21:03:19 -0600

    Added an auto subsystem

--- src/main/java/frc/robot/commands/Flywheel.java
+++ src/main/java/frc/robot/commands/Flywheel.java
@@ -1,8 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-// *import com.ctre.phoenix6.configs.TalonFXConfigurator;
-// import frc.robot.Constants;
 import frc.robot.subsystems.flywheel.FlywheelSubsystem;
 
 /** Flywheel command */
@@ -14,22 +12,22 @@ public class Flywheel extends Command {
         addRequirements(flywheelSubsystem);
     }
 
-  @Override
-  public void initialize() {
-    flywheelSubsystem.runFlywheel();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (flywheelSubsystem.Flybreak.get() == false) {
-      flywheelSubsystem.runFlywheel();
-    } else {
-      flywheelSubsystem.stopThrowing();
+    @Override
+    public void initialize() {
+        flywheelSubsystem.setFlywheelVelocity(200.0);
     }
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    @Override
+    public void execute() {
+        if (!flywheelSubsystem.getBeamBreakTriggered()) {
+            flywheelSubsystem.setFlywheelVelocity(200.0); // This value needs to be fine tuned.
+        } else {
+            flywheelSubsystem.setFlywheelVelocity(0.0);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        flywheelSubsystem.setFlywheelVelocity(0.0);
+    }
 }
