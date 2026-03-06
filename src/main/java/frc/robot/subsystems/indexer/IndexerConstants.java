55d115dcde564126490737fccd6c535e543d06f8
AJL0715 <cgathright@gmail.com>
2026-03-06 15:39:07 -0600

    updates

--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.indexer;
 
 public final class IndexerConstants {
-  public static final int INDEXER_MOTOR_ID = 0; // Example CAN ID for the indexer motor
+  public static final int INDEXER_MOTOR_ID = 13; // Example CAN ID for the indexer motor
   public static final double INDEXER_SPEED = 0.5; // Speed at which to run the indexer
   public static final int INDEXER_BEAM_BREAK_SENSOR_PORT =
       0; // Digital Input port for the beam break sensor
