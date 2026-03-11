3ea5225961c1c79bb4696c59a5f5a013799ed6bd
cgathright133 <cgathright@gmail.com>
2026-03-11 17:27:30 -0500

    updates from climb branch

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -28,7 +28,7 @@ public class Climb extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    climbSubsystem.Extend();
+   // climbSubsystem.Extend();
   }
 
   // Called once the command ends or is interrupted.
