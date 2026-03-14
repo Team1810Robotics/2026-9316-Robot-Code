aa93f72bf001e0f1569cbd37070a5047d750e407
Curtis Gathright <cgathright@gmail.com>
2026-03-14 13:16:08 -0500

    led updates into vision

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -1,16 +1,21 @@
 package frc.robot.subsystems.vision;
 
+import com.ctre.phoenix6.signals.RGBWColor;
+
 import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
+import frc.robot.subsystems.led.LEDConstants;
+import frc.robot.subsystems.led.LEDSubsystem;
 import frc.robot.util.LimelightHelpers;
 import frc.robot.util.LimelightHelpers.PoseEstimate;
 
 public class VisionSubsystem extends SubsystemBase {
   public final String limelightName;
 
+  private boolean LED_CD = false;
 
   public VisionSubsystem() {
     this.limelightName = VisionConstants.LIMELIGHT_NAME;
@@ -27,10 +32,20 @@ public class VisionSubsystem extends SubsystemBase {
       DogLog.log("Vision/TY", 0.0);
       DogLog.log("Vision/TargetID", -1);
       DogLog.log("Vision/TargetDistanceMeters", -1.0);
-      return;
+    if (!LED_CD) {
+        LEDConstants.IDLE = true;
+        LED_CD = true;
+      }
+        return;
     }
 
+    LEDConstants.IDLE = false;
+    LEDSubsystem.setLEDColor(
+        new RGBWColor(LEDConstants.PERRYWINKLE[0], LEDConstants.PERRYWINKLE[1], LEDConstants.PERRYWINKLE[2], 0),
+        false);
+    LEDSubsystem.setLEDAnimation("Rainbow", false);
 
+    LED_CD = false;
 
     DogLog.log("Vision/TargetValid", targetValid());
     DogLog.log("Vision/TX", getTx());
