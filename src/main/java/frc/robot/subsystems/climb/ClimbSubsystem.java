dc647c1c5c3da2dc840015decd5fb8a06c9cced6
Curtis Gathright <cgathright@gmail.com>
2026-03-14 13:07:39 -0500

    Bring in Ben and Will E's Climb/LED Updates

--- src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
@@ -1,6 +1,15 @@
 package frc.robot.subsystems.climb;
 
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.led.LEDConstants;
+import frc.robot.subsystems.led.LEDSubsystem;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 
 public class ClimbSubsystem extends SubsystemBase {
   // private final SparkMax motor1;
@@ -21,14 +30,23 @@ public class ClimbSubsystem extends SubsystemBase {
   }
 
   public void Extend() {
-    // motor1.set(0.25);
+  //  motor1.set(ClimbConstants.SPEED);
+  // motor2.set(-ClimbConstants.SPEED);
+  // LEDConstants.IDLE = false;
+  // LEDSubsystem.setLEDColor(
+  //     new RGBWColor(LEDConstants.GREEN[0], LEDConstants.GREEN[1], LEDConstants.GREEN[2], 0),
+  //      false);
+  //  LEDSubsystem.setLEDAnimation("Fire", false);
   }
 
   public void Stop() {
-    // motor1.set(0.0);
+ //   motor1.set(0.0);
+ //   motor2.set(0);
   }
 
   public void Retract() {
-    // motor1.set(-0.25);
+  //  motor1.set(-ClimbConstants.SPEED);
+  //  motor2.set(ClimbConstants.SPEED);
+  //  LEDConstants.IDLE = true;
   }
 }
